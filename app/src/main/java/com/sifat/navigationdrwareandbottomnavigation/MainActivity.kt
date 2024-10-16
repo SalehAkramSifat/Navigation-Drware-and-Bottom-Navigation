d917856a1f3d3295f95a6dbe8ba7eca256d14cbf
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-17 01:41:10 +0600

    Initial Commit

---
+++ app/src/main/java/com/sifat/navigationdrwareandbottomnavigation/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.sifat.navigationdrwareandbottomnavigation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sifat.navigationdrwareandbottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar.root)
+
+        val drwarelayout = binding.drawerlayout
+        val navView = binding.navigation
+
+        navController = findNavController(R.id.fragmentContainerView)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.menuFragment,
+                R.id.profileFragment
+            ), drwarelayout
+        )
+
+        setupActionBarWithNavController(navController, drwarelayout)
+        navView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)||super.onSupportNavigateUp()
+    }
+}
